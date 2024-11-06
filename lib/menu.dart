da425ef2839b91d41b9df16384bd90b78342459d
freiazulaika <freiazulaika27@gmail.com>
2024-11-06 10:40:28 +0700

    tugas 7, edit menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -7,6 +7,7 @@ class MyHomePage extends StatelessWidget {
   final String name = 'Freia Arianti Zulaika';
   final String className = 'PBP C';
 
+  // List items dari tiga item ItemHomepage, icon dan warnanya berbeda-beda
   final List<ItemHomepage> items = [
     ItemHomepage("Lihat Daftar Produk", Icons.inventory, Colors.green[300]!),
     ItemHomepage("Tambah Produk", Icons.add, Colors.teal[400]!),
@@ -15,11 +16,10 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
     return Scaffold(
-      // AppBar adalah bagian atas halaman yang menampilkan judul.
+      // Scaffold buat dasar pagenya, termasuk AppBar dan body
       appBar: AppBar(
-        // Judul aplikasi "Lahzadi".
+        // AppBar
         title: const Text(
           'Lahzadi',
           style: TextStyle(
@@ -27,17 +27,14 @@ class MyHomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        // Menyusun widget secara vertikal dalam sebuah kolom.
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Row untuk menampilkan 3 InfoCard secara horizontal.
+            // Baris pertama berisi tiga InfoCard untuk NPM, Nama, dan Kelas
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -46,17 +43,11 @@ class MyHomePage extends StatelessWidget {
                 InfoCard(title: 'Class', content: className),
               ],
             ),
-
-            // Memberikan jarak vertikal 16 unit.
-            const SizedBox(height: 16.0),
-
-            // Menempatkan widget berikutnya di tengah halaman.
+            const SizedBox(
+                height: 16.0), // Space antara bagian infoCard dengan welcome
             Center(
               child: Column(
-                // Menyusun teks dan grid item secara vertikal.
-
                 children: [
-                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
@@ -67,18 +58,15 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                   ),
-
-                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
+                  // GridView untuk menampilkan daftar item dengan dalam bentuk 3 kolom
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
-                    // Agar grid menyesuaikan tinggi kontennya.
                     shrinkWrap: true,
-
-                    // Menampilkan ItemCard untuk setiap item dalam list items.
+                    // Bikin setiap item dalam daftar `items` menjadi ItemCard
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
@@ -93,6 +81,7 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+// Buat item di halaman utamanya, propertinya ada name, icon, dan color
 class ItemHomepage {
   final String name;
   final IconData icon;
@@ -102,34 +91,27 @@ class ItemHomepage {
 }
 
 class ItemCard extends StatelessWidget {
-  // Menampilkan kartu dengan ikon dan nama.
-
   final ItemHomepage item;
   const ItemCard(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      // Menentukan warna latar belakang dari tema aplikasi.
       color: item.color,
-      // Membuat sudut kartu melengkung.
       borderRadius: BorderRadius.circular(12),
-
       child: InkWell(
-        // Aksi ketika kartu ditekan.
+        // Buat interaktif
         onTap: () {
-          // Menampilkan pesan SnackBar saat kartu ditekan.
+          // Menampilkan SnackBar saat item ditekan
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
         },
-        // Container untuk menyimpan Icon dan Text
         child: Container(
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
-              // Menyusun ikon dan teks di tengah kartu.
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
@@ -153,31 +135,25 @@ class ItemCard extends StatelessWidget {
 }
 
 class InfoCard extends StatelessWidget {
-  // Kartu informasi yang menampilkan title dan content.
-
-  final String title; // Judul kartu.
-  final String content; // Isi kartu.
+  final String title;
+  final String content;
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        // Mengatur ukuran dan jarak di dalam kartu.
-        width: MediaQuery.of(context).size.width /
-            3.5, // menyesuaikan dengan lebar device yang digunakan.
+        width: MediaQuery.of(context).size.width / 3.5,
         padding: const EdgeInsets.all(16.0),
-        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
               title,
               style: const TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 8.0),
+            const SizedBox(height: 8.0), // Spasi antara title dan content
             Text(content),
           ],
         ),
